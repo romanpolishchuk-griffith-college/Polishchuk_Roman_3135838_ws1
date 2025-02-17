9959a3d5bcb82f6f755727be8352f0f43d9043e6
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-02-17 04:34:29 +0000

    Add stub tests and word methods

---
+++ src/griffith/Word.java
@@ -0,0 +1,22 @@
+package griffith;
+
+public class Word {
+	
+	private char[] letters;
+	
+	public Word(char[] letters) {
+		this.letters = letters;
+	}
+	
+	boolean contains(char symbol) {
+		return false;	
+	}
+	
+	int length() {
+		return 0;
+	}
+	
+	char[] getLetters() {
+		return null;
+	}
+}
---
+++ src/griffith/WordTest.java
@@ -0,0 +1,23 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordTest {
+
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testLength() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testNotNull() {
+		fail("Not yet implemented");
+	}
+}
