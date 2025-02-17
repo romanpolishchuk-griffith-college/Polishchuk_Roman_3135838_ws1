cbe13a9e16e4310a470971e1a2964acd81566788
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-02-17 00:48:31 +0000

    Add testGradesMax test

--- src/griffith/GradesTest.java
+++ src/griffith/GradesTest.java
@@ -8,16 +8,36 @@ class GradesTest {
 
 	@Test
     void testGradesMax(){
-		fail("Not yet implemented");
+		//Create grades class instance
+		Grades grades = new Grades();
+		
+		//Test max is the first element
+		assertEquals(10, grades.gradesMax(new int[]{10, 6, 3, 1, 2}));
+		//Test max is the last element
+		assertEquals(5, grades.gradesMax(new int[]{0, 4, 3, 1, 5}));
+		//Test max is in the middle
+		assertEquals(30, grades.gradesMax(new int[]{3, 30, 3, 29, 2}));
+		//Test max is the negative number
+		assertEquals(-2, grades.gradesMax(new int[]{-6, -26, -3, -2, -20}));
+		
+		//For null and empty array method throw exception
+		
+		//Test null
+		assertThrows(Exception.class, () -> grades.gradesMax(null));
+		//Test empty array
+		assertThrows(Exception.class, () -> grades.gradesMax(null));
 	};
+	
     @Test
     void testGradesTotal(){
     	fail("Not yet implemented");
     };
+    
     @Test
     void testGradesAverage(){
     	fail("Not yet implemented");
     };
+    
     @Test
     void testCountFails(){
     	fail("Not yet implemented");
