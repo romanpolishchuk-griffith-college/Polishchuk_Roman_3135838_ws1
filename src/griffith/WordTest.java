bcfd21accb760eab868bc96b85fefe25d823af54
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-02-17 21:15:04 +0000

    Add testContains method

--- src/griffith/WordTest.java
+++ src/griffith/WordTest.java
@@ -8,7 +8,25 @@ class WordTest {
 
 	@Test
 	void testContains() {
-		fail("Not yet implemented");
+		//Create word class instance
+		Word word1 = new Word(new char[] {'a', 'b', 'c'});
+		//Test word contains a
+		assertTrue(word1.contains('a'));
+		
+		//Create word class instance
+		Word word2 = new Word(new char[] {'A', 'B', 'C'});
+		//Test word contains A
+		assertTrue(word2.contains('a'));
+		
+		//Create word class instance
+		Word word3 = new Word(new char[] {'a', 'b', 'c'});
+		//Test word contains z
+		assertTrue(word3.contains('z'));
+		
+		//Create word class instance
+		Word word4 = new Word(new char[] {});
+		//Test word contains a
+		assertTrue(word4.contains('a'));
 	}
 	
 	@Test
